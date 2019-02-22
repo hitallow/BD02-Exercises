86c3a13b69b1ba420c457f806453b8afc573e6e0
Hítallo William <42446670+hitallow@users.noreply.github.com>
2019-02-22 15:05:47 -0300

    creates tables

---
+++ creates aula de BD02 - 01.sql
@@ -0,0 +1,46 @@
+create table atividade(
+	codigo int not null primary key,
+    descricao varchar(100)
+    );
+create table funcionario(
+	codigo int not null primary key,
+    nome varchar(30),
+    sexo varchar(1) ,
+    dtNascimento date not null,
+    salario float not null,
+    codDept int not null
+    );
+create table departamento(
+	codigo int not null primary key,
+    descricao varchar(100) not null,
+    codFuncionarioGerente int not null
+	);
+create table projeto(
+	codigo int not null primary key,
+    descricao varchar(100),
+    codDept int not null,
+    codFuncionarioResponsavel int not null,
+    dataInicio date not null,
+    dataFim date not null
+);
+create table AtividadeProjeto(
+	id int not null primary key AUTO_INCREMENT,
+	codProjeto int not null,
+    codAtividade int not null,
+    dataInicio date not null,
+    dataFim date not null
+);
+/** adcionando as FKs**/
+alter table funcionario add constraint funcDeptFK foreign key (codDept) references departamento(codigo);
+alter table departamento add constraint depGerFK foreign key (codFuncionarioGerente) references funcionario(codigo);
+alter table projeto add constraint projDeptFK foreign key (codDept) references departamento(codigo);
+alter table projeto add constraint projetoFuncFK foreign key (codFuncionarioResponsavel) references funcionario(codigo);
+alter table AtividadeProjeto add constraint AtvProProjFK foreign key (codProjeto) references projeto(codigo);
+alter table AtividadeProjeto add constraint AtvProAtvFK foreign key (codAtividade) references atividade(codigo);
+
+insert into atividade values(1, 'Descrição da atividade 1 ');
+insert into atividade values(2, 'Descrição da atividade 2');
+insert into atividade values(3, 'Descrição da atividade 3 ');
+insert into atividade values(4, 'Descrição da atividade 4 ');
+insert into atividade values(5, 'Descrição da atividade 5 ');
+
